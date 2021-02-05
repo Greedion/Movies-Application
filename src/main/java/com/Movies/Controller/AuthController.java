0c997c87fb6c901f24b4f4a0f8296b2940e28cff
greedion <adrian43graf@gmail.com>
2021-02-05 15:04:21 +0100

    Add Swagger 2, Add missing validation addnotation
    Added Swagger config class and api info, and missed @Valid addnotations

--- src/main/java/com/Movies/Controller/AuthController.java
+++ src/main/java/com/Movies/Controller/AuthController.java
@@ -3,14 +3,21 @@ package com.Movies.Controller;
 import com.Movies.POJO.POJOUser;
 import com.Movies.Security.JWTAuth.JWTUtils;
 import com.Movies.Service.Authorization.AuthorizationServiceImpl;
+import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
@@ -30,18 +37,33 @@ public class AuthController {
         this.jwtUtils = jwtUtils;
     }
 
+    @ApiOperation(value = "Log in.", notes = "Default account's : Admin/Admin User/User")
     @PostMapping("/signin")
     @PreAuthorize("permitAll()")
-    ResponseEntity<?> login(@RequestBody POJOUser pojoUser) {
+    ResponseEntity<?> login(@Valid @RequestBody POJOUser pojoUser, BindingResult result) {
         if(pojoUser==null){
             logger.error("Attempt login with empty input data.");
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Attempt login with empty input data");
         } else if(pojoUser.getUsername() == null || pojoUser.getPassword() == null){
             logger.error("Attempt login with empty input data.");
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Attempt login with empty input data");
-        }else {
+        }else if (result.hasErrors()) {
+            logger.error("Attempt to sing in with wrong data structure.");
+            return new ResponseEntity<>(hadErrors(result), HttpStatus.BAD_REQUEST);
+        }
+        else {
             return authorizationService.start(pojoUser, authenticationManager);
         }
+    }
+
+    Map<String, String> hadErrors(BindingResult result) {
+        Map<String, String> errorMap = new HashMap<>();
+
+        for (FieldError error : result.getFieldErrors()
+        ) {
+            errorMap.put(error.getField(), error.getDefaultMessage());
+        }
+        return errorMap;
     }
 
 }
---
+++ src/main/java/com/Movies/Security/SwaggerConfiguration.java
@@ -0,0 +1,54 @@
+package com.Movies.Security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.*;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Collections;
+import java.util.List;
+
+@Configuration
+public class SwaggerConfiguration {
+
+    @Bean
+    public Docket getDocket() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .securityContexts(Collections.singletonList(securityContext()))
+                .securitySchemes(Collections.singletonList(apiKey()))
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.Movies.Controller"))
+                .build().apiInfo(createApiInfo());
+    }
+
+    private ApiInfo createApiInfo() {
+        return new ApiInfo("Movies Api",
+                null,
+                "1.00",
+                "https://github.com/Greedion/Movies-Application/blob/master/README.md",
+                new Contact("Adrian Russu",
+                        "https://github.com/Greedion/",
+                        "adrian43graf@gmail.com"),
+                "my own licence",
+                "https://github.com/Greedion/Movies-Application/blob/master/README.md",
+                Collections.emptyList());
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey("JWT", "Authorization", "header");
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder().securityReferences(defaultAuth()).build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Collections.singletonList(new SecurityReference("JWT", authorizationScopes));
+    }
+}
